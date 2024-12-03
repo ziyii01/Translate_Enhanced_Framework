8464183e76b71134d7d3a4162d2ceb44ffa61bcb
op200 <112297583+op200@users.noreply.github.com>
2024-12-03 16:06:04 +0800

    更新 main.py

--- src/main.py
+++ src/main.py
@@ -1,34 +1,68 @@
 import os
+import sys
+import locale
+import codecs
 
 import chardet
 import httpx
 from loguru import logger as log
+log.remove()
+log.add(sys.stderr, format="<green>{time:YYYY.MM.DD HH:mm:ss.SS}</green><blue><level> [{level}] {message}</level></blue>", level="INFO")
+
 
 URL_ROOT = "https://api.zhconvert.org"
 
 
-def current_running_path() -> str:
-    return os.path.dirname(os.path.abspath(__file__))
 
-
-def get_all_zh_hans_ass_files_in_current_path() -> list[str]:
+def get_all_ass_path(dir_path: str) -> list[str]:
     return [
-        os.path.join(current_running_path(), f)
-        for f in os.listdir(current_running_path())
-        if f.endswith(".zh-Hans.ass")
+        os.path.join(dir_path, filename)
+        for filename in os.listdir(dir_path)
+        if filename.endswith(".ass")
     ]
+
+def match_suffix_in_list(all_list: list[str], suffix: str) -> list[str]:
+    return [
+        pathname
+        for pathname in all_list
+        if pathname.endswith(suffix)
+    ]
+
+def get_encoding_by_text(pathname: str) -> str | None:
+    with open(pathname, "rb") as file:
+        text_data = file.read()
+        encoding = chardet.detect(text_data)["encoding"]
+
+    if encoding == None:
+        log.warning(f'Can not find the encoding from "{pathname}", auto use UTF-8')
+        return 'utf8'
+
+    if encoding == 'GB2312' or encoding == 'cp936':
+        encoding = 'gb18030-2000'
+    
+    if not encoding.startswith("UTF") and not encoding == 'gb18030-2000':
+        locale_encoding = locale.getencoding()
+        log.warning(f'Auto find the encoding is {encoding}, it may be {locale_encoding} ({codecs.lookup(locale_encoding).name}), has been auto changed it')
+        encoding = locale_encoding
+    
+    log.info(f"Auto find the encoding is {encoding} ({codecs.lookup(encoding).name})")
+    return encoding
 
 
 class Zhconvert:
     @staticmethod
     def convert(
-        api,
-        text,
-        converter="Traditional",
-        apiKey="",
-        outputFormat="json",
-        prettify="false",
+        text: str,
+        converter: str,
+        apiKey = "",
+        outputFormat = "json",
+        prettify = "false",
     ) -> httpx.Response:
+        """
+        converter:
+        Simplified、 Traditional、 China、 Hongkong、 Taiwan、 Pinyin （拼音化） Bopomofo （注音化）、 Mars （火星化）、 WikiSimplified （維基簡體化）、 WikiTraditional （維基繁體化）
+        """
+        api = '/convert'
         return httpx.post(
             f"{URL_ROOT}/{api}",
             data={
@@ -39,46 +73,144 @@ class Zhconvert:
                 "prettify": prettify,
             },
         )
+    
+    @staticmethod
+    def find_langTag_by_converter(converter: str) -> str | None:
+        lang_tag: str | None = None
+
+        if converter == "Simplified":
+            lang_tag = "zh-Hans"
+        elif converter == "Traditional":
+            lang_tag = "zh-Hant"
+        elif converter == "Hongkong":
+            lang_tag = "zh-Hant-HK"
+        elif converter == "Taiwan":
+            lang_tag = "zh-Hant-TW"
+
+        return lang_tag
+
+    @staticmethod
+    def translate(input_pathname: str, converter: str) -> bool:
+        if not os.path.exists(input_pathname):
+            log.error(f'The input file "{input_pathname}" is not exist')
+            return False
+        
+        encoding = get_encoding_by_text(input_pathname)
+        
+        with open(input_pathname, "rt", encoding = encoding) as file:
+            text_data = file.read()
+        response = Zhconvert.convert(text_data, converter)
+        if response.status_code != 200:
+            log.error(f"网址请求失败: {response.text}")
+            return False
+        response_json_data = response.json()
+        if response_json_data["code"] != 0:
+            log.error(f"繁化姬接口失败: {response_json_data['msg']}")
+            return False
+        else:
+            last_dot_index = input_pathname.rfind('.')
+            second_last_dot_index = input_pathname.rindex('.', 0, last_dot_index)
+            output_pathname = f'{input_pathname[:second_last_dot_index]}.{Zhconvert.find_langTag_by_converter(converter)}.ass'
+
+            if os.path.exists(output_pathname):
+                log.warning(f'The output file "{output_pathname}" already exists, auto overwrite it')
+
+            with open(output_pathname, "wt", encoding = 'utf_8_sig') as file:
+                file.write(response_json_data["data"]["text"])
+            
+            log.info(f'Translate the file "{input_pathname}" to "{output_pathname}" success')
+            return True
+
+
+def run_command(command: str):
+    global dir_path, all_ass_list, zh_hans_ass_list
+    
+    command = command.lower().strip()
+
+    if command == "help":
+        print(
+            "Help:\n"
+            "  You can input command or use the argument value to run"
+            "\n"
+            "Commands:\n"
+            "  exit\n"
+            "    Exit this program\n"
+            "  list\n"
+            "    Show found list\n"
+            "  flush\n"
+            "    Search the list again\n"
+            "  tr all / translate all\n"
+            "    Translate all zh-Hans ass to zh-Hant\n"
+            "  cd <string>\n"
+            "    Change current path\n"
+            "  cls / clear\n"
+            "    Clear screen\n"
+            "  $ <string>\n"
+            "    Execute the code string directly after the $\n"
+            '    The string "\\N" will be changed to real "\\n"\n'
+            "  <string>\n"
+            "    Translate a file with matching prefixe (e.g. ep num) into zh-Hant\n"
+        )
+
+    elif command == "exit":
+        exit()
+    
+    elif command == "list":
+        show_list(all_ass_list, "All ASS files")
+        show_list(zh_hans_ass_list, "All zh-Hans ASS files")
+
+    elif command == "flush":
+        all_ass_list, zh_hans_ass_list = get_zh_hans_ass_list()
+
+    elif command == "tr all" or command == "translate all":
+        for pathname in zh_hans_ass_list:
+            Zhconvert.translate(pathname, 'Traditional')
+
+    elif command[:2] == "cd":
+        new_path = command.replace("cd","").strip()
+        if os.path.isdir(new_path):
+            os.chdir(new_path)
+            dir_path = os.getcwd()
+        else:
+            log.error(f'The path "{new_path}" has error')
+
+    elif command == "cls" or command == "clear":
+        if os.name == 'nt':
+            os.system('cls')
+        else:
+            os.system('clear')
+
+    elif command[0] == '$':
+        exec(command[1:].lstrip().replace(r"\N","\n"))
+
+    else:
+        pathname = command + ".zh-Hans.ass"
+        if os.path.exists(pathname):
+            Zhconvert.translate(pathname, 'Traditional')
+        else:
+            log.warning(f'Can not find the file "{pathname}", cancel the translation')
 
 
 if __name__ == "__main__":
+    # get_encoding_by_text(r'c:\Users\op200\Documents\test\新建 文本文档.txt')
+
+    dir_path = os.getcwd()
+
+    def get_zh_hans_ass_list():
+        all_ass_list = get_all_ass_path(dir_path)
+        zh_hans_ass_list = match_suffix_in_list(all_ass_list, ".zh-Hans.ass")
+        return all_ass_list, zh_hans_ass_list
+    
+    def show_list(list: list[str], list_name: str):
+        print(f'{list_name}:')
+        for str in list:
+            print(f'  {str}')
+
+    
+    all_ass_list, zh_hans_ass_list = get_zh_hans_ass_list()
+    
+    for argv in sys.argv[1:]:
+        run_command(argv)
+
     while True:
-        episode_number: str = input("请输入你要的集数: ")
-        for file_name in get_all_zh_hans_ass_files_in_current_path():
-            # 判断是否是当前集数
-            if file_name.endswith(f"{episode_number}.zh-Hans.ass"):
-                log.info(f"正在处理文件: {file_name}")
-                # 读取ass二进制，用于判断文字编码
-                with open(f"{file_name}", "rb") as f:
-                    text_data = f.read()
-                    encoding = chardet.detect(text_data)["encoding"]
-                # 判断文字编码是否正确
-                while True:
-                    if (
-                        encoding_input := input(
-                            f"当前的文字编码识别为{encoding}，正确请回车，不正确请输入文字编码: "
-                        )
-                    ) != "":
-                        encoding = encoding_input
-                    else:
-                        break
-                # 读取ass文件内容
-                with open(f"{file_name}", "r", encoding="utf-8") as f:
-                    text_data = f.read()
-                # 调用繁化姬接口
-                response = Zhconvert.convert("/convert", text_data)
-                if response.status_code != 200:
-                    log.error(f"网址请求失败: {response.text}")
-                    continue
-                Response_json_data = response.json()
-                if Response_json_data["code"] != 0:
-                    log.error(f"繁化姬接口失败: {Response_json_data['msg']}")
-                else:
-                    # 写入文件
-                    with open(
-                        f"{episode_number.zfill(2)}.zh-Hant.ass",
-                        "w",
-                        encoding=encoding,
-                    ) as f:
-                        f.write(Response_json_data["data"]["text"])
-                    log.info(f"文件: {file_name} 处理完成")
+        run_command(input(f'{dir_path}> Input command>'))
